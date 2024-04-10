5390733788c5d603858dbcff7bf6b91b431fefdf
hp <hp@DESKTOP-CROQ9O6>
2024-04-10 09:43:11 +0530

    Add project files.

---
+++ _Partial_View_/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_Partial_View_.MvcApplication" Language="C#" %>
---
+++ _Partial_View_/Models/PartialController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _Partial_View_.Models
+{
+    public class PartialController : Controller
+    {
+        // GET: Partial
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
